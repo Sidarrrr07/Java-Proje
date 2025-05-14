96acbe2d0f3da70ba980dc36fb825aa47723020e
Sidarrrr07 <s.ylmz_07@hotmail.com>
2025-05-14 03:01:10 +0300

    Kitap.java

---
+++ Kitap.java
@@ -0,0 +1,19 @@
+package KitaplikYonetimSistemiOOP;
+
+
+
+public class Kitap extends KitapYonetim {
+	
+	
+	 String kitapAdi;
+	    int fiyat;
+	    int yayinYili;
+
+	    public Kitap(String kitapAdi, int fiyat, int yayinYili) {
+	        this.kitapAdi = kitapAdi;
+	        this.fiyat = fiyat;
+	        this.yayinYili = yayinYili;
+	    }
+	
+	
+	}
